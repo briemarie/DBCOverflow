324b11dc3801a303c4bc362ebd2026a64d8eeeed
briemarie <thebriemarie@gmail.com>
2015-01-12 23:26:34 -0800

    continuation of last commit, some filed didn't add

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,4 +1,25 @@
 class QuestionsController < ApplicationController
-  def index
-  end
+    def index
+        @questions = Question.all
+    end
+
+    def show
+        @question = Question.find(params[:id])
+        @answers = Answer.all.find_by(question_id: @question.id)
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
