2ccb09e64085ba26b3283d943e5b63dfe89da417
briemarie <thebriemarie@gmail.com>
2015-01-12 23:53:01 -0800

    Chnaged gems and requirements, not sure if it works this time though

--- dbc_overflow/config/application.rb
+++ dbc_overflow/config/application.rb
@@ -7,7 +7,16 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
+
+#for testing
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'capybara/rails'
+require 'capybara/rspec'
+
+
 # require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
